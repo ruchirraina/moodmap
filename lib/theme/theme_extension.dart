b463b18b8e6e96b8604ab7c6fd3bec151826e4ef
ruchir <ruchirraina@gmail.com>
2026-08-27 18:08:52 +0530

    chore: remove obsolete files from old project structure

--- lib/theme/theme_extension.dart
+++
@@ -1,8 +0,0 @@
-// for improving code readability
-import 'package:flutter/material.dart';
-
-extension ThemeExtension on BuildContext {
-  ThemeData get theme => Theme.of(this);
-  TextTheme get textTheme => Theme.of(this).textTheme;
-  ColorScheme get colorScheme => Theme.of(this).colorScheme;
-}
